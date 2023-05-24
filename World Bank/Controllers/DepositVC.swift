f7a86d0eec8d26a9b9f6975f52a447ff41bc4521
hassanesidibe <hassanesidibe5@gmail.com>
2023-05-24 19:02:09 -0400

    DepositVC shows red x mark when user enters an invalid amount.

--- World Bank/Controllers/DepositVC.swift
+++ World Bank/Controllers/DepositVC.swift
@@ -8,6 +8,7 @@
 import UIKit
 import FirebaseFirestore
 import FirebaseAuth
+import AVFoundation
 
 protocol DepositVCManager {
     func didFinishDepositingMoneyToAccount(_ depositVC: DepositVC)
@@ -20,6 +21,12 @@ class DepositVC: UIViewController {
     let db = Firestore.firestore()
     var banAccount: BankAccountManager?
     var signedInUserEmail: String?
+    var soundPlayer: AVAudioPlayer?
+    var timer = Timer() //To control how long the payment sound should be played
+    
+    
+    @IBOutlet weak var checkmark: UIImageView!
+    @IBOutlet weak var xmark: UIImageView!
     
 //    HomeVC's prepare for segue method will assign a value to this bank account variable below
     var bankAccount: BankAccountManager?
@@ -40,11 +47,16 @@ class DepositVC: UIViewController {
                 messageLabel.text = "Credit card (XXXX)"
             }
         }
+        
+        hideCheckmarkAndXmark()
     }
     
 
     
     @IBAction func depositButtonPressed(_ sender: UIButton) {
+        
+//        I HAVE TO ADD A CHECK MARK or Xmark TO TransferVC VIEW WHEN transfer button is pressed, I also need to implement the red xmark for credit card payment. The credit card payment already has a greene check mark, i only need add a red check mark to it
+        
         if Auth.auth().currentUser != nil {
             if let userEmail = Auth.auth().currentUser?.email {
                 signedInUserEmail = userEmail
@@ -55,13 +67,18 @@ class DepositVC: UIViewController {
                         if let unwrappedBankAccount = self.bankAccount {
                             if accountType == .checking {
                                 unwrappedBankAccount.deposit(amount, to: .checking)
-                                
+                                showCheckMarkAndPlaySound()
+                                dismissViewAfter(numberOfSeconds: 2)
+                                        
                             } else if accountType == .savings {
                                 unwrappedBankAccount.deposit(amount, to: .savings)
+                                showCheckMarkAndPlaySound()
+                                dismissViewAfter(numberOfSeconds: 2)
                             }
-                            
-                            self.dismiss(animated: true, completion: nil)
                         }
+                        
+                    } else {
+                        showXmarkFor(numberOfSeconds: 1)
                     }
                 }
                 
@@ -72,7 +89,74 @@ class DepositVC: UIViewController {
         }
         
     }
-    
-    
 }
+
+
+//Play sound and dismiss view after deposit
+extension DepositVC {
+    
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "dingSound", ofType:"mp3") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: url)
+            soundPlayer?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    //Dismiss CreditCardPaymentVC after the specified number of seconds
+    func dismissViewAfter(numberOfSeconds: Int) {
+        var secondsPassed = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if secondsPassed < numberOfSeconds {
+                secondsPassed += 1
+            } else {
+                self.timer.invalidate()
+                self.dismiss(animated: true)
+            }
+        }
+    }
+}
+
+
+//MARK: - Show and hide checkmark
+extension DepositVC {
+    
+    func hideCheckmarkAndXmark() {
+        checkmark.isHidden = true
+        xmark.isHidden = true
+    }
+    
+    func showCheckMarkAndPlaySound() {
+        checkmark.isHidden = false
+        xmark.isHidden = true
+        playSound()
+    }
+    
+    func showXmarkFor(numberOfSeconds: Int) {
+        checkmark.isHidden = true
+        xmark.isHidden = false
+        self.timer.invalidate()
+        
+        var secondsPassed = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if secondsPassed < numberOfSeconds {
+                secondsPassed += 1
+            } else {
+                self.timer.invalidate()
+                self.xmark.isHidden = true
+            }
+        }
+    }
+}
+
+
+
+
+
 
